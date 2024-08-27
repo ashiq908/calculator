35762bdb489398492a89764342f7fb4b2382070d
ashiq908 <ashikashok37@gmail.com>
2024-08-27 12:07:50 +0530

    page craeated

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function sum(){
+    var fnum = parseInt(document.getElementById('num1').value)
+    var snum = parseInt(document.getElementById('num2').value)
+    alert(fnum+snum)
+}
+function sub(){
+    var fnum = parseInt(document.getElementById('num1').value)
+    var snum = parseInt(document.getElementById('num2').value)
+    alert(fnum-snum)
+}
+function mul(){
+    var fnum = parseInt(document.getElementById('num1').value)
+    var snum = parseInt(document.getElementById('num2').value)
+    alert(fnum*snum)
+}
+function div(){
+    var fnum = parseInt(document.getElementById('num1').value)
+    var snum = parseInt(document.getElementById('num2').value)
+    alert(fnum/snum)
+}
